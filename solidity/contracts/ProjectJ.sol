e8f36a623aabfc9faa8cdcfbb5615b0f7cb45604
BowTiedPickle <bowtiedpickle@protonmail.com>
2021-11-07 19:24:44 -0600

    Working contract, working blacklist implementation

--- solidity/contracts/ProjectJ.sol
+++ solidity/contracts/ProjectJ.sol
@@ -2,33 +2,40 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract ProjectJ is ERC721PresetMinterPauserAutoId {
-    // Placeholder, base URI has to be set with this preset OZ contract
-    string _baseURI;
-    constructor() ERC721("Project J","PRJ",_baseURI) {
-    }
+contract ProjectJ is ERC721 {
 
     // Declare roles for AccessControl
-    bytes32 public constant MODERATOR = keccak256("MODERATOR");
+    bytes32 public constant MODERATOR_ROLE = keccak256("MODERATOR_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor() ERC721("ProjectJ","PRJ") {}
 
     // Mapping of good standing to allow moderation of access control
-    mapping(address => bool) goodStanding = true;
+    mapping(address => bool) public blacklist;
 
     // Modification of standing will emit target address, the new standing, and the address changing the standing
     event StandingModified(address target, bool newStanding, address changedBy);
 
     modifier inGoodStanding() {
-        require(goodStanding[msg.sender] == true,"Account %s is not in good standing.",msg.sender);
+        require(blacklist[msg.sender] == false,"Account is blacklisted.");
         _;
     }
 
     // Modify the standing of the target address. Cannot change own standing. Requires moderator role.
-    function modifyStanding(address target, bool newStanding) public onlyRole(MODERATOR) {
-        require(target != msg.sender,"Moderator cannot modify their own standing.");
-        goodStanding[target] = newStanding;
+    function modifyStanding(address target, bool newStanding) inGoodStanding public {
+        require(target != msg.sender,"User cannot modify their own standing.");
+        blacklist[target] = newStanding;
         emit StandingModified(target, newStanding, msg.sender);
+    }
+
+    function checkStanding(address _address) public view returns (bool) {
+        return blacklist[_address];
     }
 
 }
--- solidity/scripts/run.js
+++ solidity/scripts/run.js
@@ -3,6 +3,16 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
+
+    const tester1 = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+    const tester2 = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    let txn;
+    txn = await nftContract.modifyStanding(tester1,true)
+    console.log("Standing modified")
+
+    let standing;
+    standing = await nftContract.checkStanding(tester1)
+    console.log(standing)
 };
   
 const runMain = async () => {
