2d3e5bb553e4860f99a0f5f5a3fa2b0357bc4868
BowTiedPickle <bowtiedpickle@protonmail.com>
2021-11-07 20:19:14 -0600

    Implemented rudimentary access control

--- solidity/contracts/ProjectJ.sol
+++ solidity/contracts/ProjectJ.sol
@@ -6,15 +6,34 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+// import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 
-contract ProjectJ is ERC721 {
+contract ProjectJ is ERC721, AccessControlEnumerable {
 
     // Declare roles for AccessControl
     bytes32 public constant MODERATOR_ROLE = keccak256("MODERATOR_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    constructor() ERC721("ProjectJ","PRJ") {}
+    constructor(
+        address[] memory _moderators,
+        address[] memory _pausers
+    ) ERC721("ProjectJ","PRJ") {
+
+        // Initialize default admin role
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        // Initialize moderators
+        uint256 i;
+        for (i = 0;i < _moderators.length; i++) {
+            _setupRole(MODERATOR_ROLE,_moderators[i]);
+        }
+
+        // Initialize pausers
+        for (i = 0;i < _pausers.length; i++) {
+            _setupRole(PAUSER_ROLE,_pausers[i]);
+        }
+    }
 
     // Mapping of good standing to allow moderation of access control
     mapping(address => bool) public blacklist;
@@ -28,14 +47,28 @@ contract ProjectJ is ERC721 {
     }
 
     // Modify the standing of the target address. Cannot change own standing. Requires moderator role.
-    function modifyStanding(address target, bool newStanding) inGoodStanding public {
+    function modifyStanding(address target, bool newStanding) inGoodStanding onlyRole(MODERATOR_ROLE) public {
         require(target != msg.sender,"User cannot modify their own standing.");
         blacklist[target] = newStanding;
         emit StandingModified(target, newStanding, msg.sender);
     }
 
+    // Return standing of target address.
     function checkStanding(address _address) public view returns (bool) {
         return blacklist[_address];
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(AccessControlEnumerable, ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
 }
--- solidity/scripts/run.js
+++ solidity/scripts/run.js
@@ -1,17 +1,20 @@
 const main = async () => {
+    const tester2 = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+    const tester1 = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    var moderators = [tester1,tester2]
+    var pausers = [tester1,tester2]
+
     const nftContractFactory = await hre.ethers.getContractFactory('ProjectJ');
-    const nftContract = await nftContractFactory.deploy();
+    const nftContract = await nftContractFactory.deploy(moderators,pausers);
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
 
-    const tester1 = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
-    const tester2 = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
     let txn;
-    txn = await nftContract.modifyStanding(tester1,true)
+    txn = await nftContract.modifyStanding(tester2,true)
     console.log("Standing modified")
 
     let standing;
-    standing = await nftContract.checkStanding(tester1)
+    standing = await nftContract.checkStanding(tester2)
     console.log(standing)
 };
   
