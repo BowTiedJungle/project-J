89bb3c6d7285f5287f4d28c5d1594311f9a5515c
BowTiedPickle <bowtiedpickle@protonmail.com>
2021-11-11 09:37:47 -0600

    Updated for clarity, finished modifyStanding tests

--- solidity/test/test.js
+++ solidity/test/test.js
@@ -3,9 +3,9 @@ const { ethers } = require("hardhat");
 const { waffle } = require("hardhat");
 
 const provider = waffle.provider;
-const [tester1, tester2, tester3] = provider.getWallets();
-var moderators = [tester1.address];
-var pausers = [tester1.address];
+const [mod1, mod2, citizen1, citizen2] = provider.getWallets();
+var moderators = [mod1.address,mod2.address];
+var pausers = [mod1.address,mod2.address];
 const baseURI = "testURI";
 
 describe("ProjectJ", function () {
@@ -16,12 +16,12 @@ describe("ProjectJ", function () {
         const projectJ = await ProjectJ.deploy(moderators,pausers,baseURI);
         await projectJ.deployed();
 
-        expect(await projectJ.checkStanding(tester2.address)).to.equal(false);
+        expect(await projectJ.checkStanding(citizen1.address)).to.equal(false);
 
-        const setBlacklist = await projectJ.modifyStanding(tester2.address,true);
+        const setBlacklist = await projectJ.modifyStanding(citizen1.address,true);
         await setBlacklist;
 
-        expect(await projectJ.checkStanding(tester2.address)).to.equal(true);
+        expect(await projectJ.checkStanding(citizen1.address)).to.equal(true);
 
     });
 
@@ -31,11 +31,41 @@ describe("ProjectJ", function () {
         const projectJ = await ProjectJ.deploy(moderators,pausers,baseURI);
         await projectJ.deployed();
 
-        expect(await projectJ.checkStanding(tester3.address)).to.equal(false);
+        expect(await projectJ.checkStanding(citizen1.address)).to.equal(false);
 
-        await expect(projectJ.connect(tester2).modifyStanding(tester3.address,true)).to.be.reverted;
+        await expect(projectJ.connect(citizen2).modifyStanding(citizen1.address,true)).to.be.reverted;
 
-        expect(await projectJ.checkStanding(tester3.address)).to.equal(false);
+        expect(await projectJ.checkStanding(citizen1.address)).to.equal(false);
+
+    });
+
+    it("Should NOT allow a user to change their own blacklist status", async function () {
+
+        const ProjectJ = await ethers.getContractFactory("ProjectJ");
+        const projectJ = await ProjectJ.deploy(moderators,pausers,baseURI);
+        await projectJ.deployed();
+
+        expect(await projectJ.checkStanding(mod1.address)).to.equal(false);
+
+        await expect(projectJ.modifyStanding(mod1.address,true)).to.be.reverted;
+
+        expect(await projectJ.checkStanding(mod1.address)).to.equal(false);
+
+    });
+
+    it("Should NOT allow a blacklisted moderator to change a blacklist status", async function () {
+
+        const ProjectJ = await ethers.getContractFactory("ProjectJ");
+        const projectJ = await ProjectJ.deploy(moderators,pausers,baseURI);
+        await projectJ.deployed();
+
+        expect(await projectJ.checkStanding(mod2.address)).to.equal(false);
+        await projectJ.modifyStanding(mod2.address,true);
+        expect(await projectJ.checkStanding(mod2.address)).to.equal(true);
+
+        await expect(projectJ.connect(mod2).modifyStanding(citizen1.address,true)).to.be.reverted;
+
+        expect(await projectJ.checkStanding(citizen1.address)).to.equal(false);
 
     });
 
