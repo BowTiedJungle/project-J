530d33909bec904dcc108b450ae473ba41397d8c
BowTiedPickle <bowtiedpickle@protonmail.com>
2021-12-17 22:32:56 -0600

    Updated run.js to interact with deployed proxy

--- solidity/scripts/run.js
+++ solidity/scripts/run.js
@@ -1,40 +1,15 @@
 const main = async () => {
-    // Hardhat generates a deterministic set of testnet addresses for the local dev chain. Txns default to being sent by first one in this context.
-    const [tester1, tester2, degen1, degen2] = await hre.ethers.getSigners();
-    var moderators = [tester1.address];
-    var pausers = [tester1.address];
-    var degens = [degen1.address,degen2.address]
-    const baseURI = "testURI/";
 
+    // Get ABI
+    const ProjectJ = await hre.ethers.getContractFactory('ProjectJTest');
 
-    const nftContractFactory = await hre.ethers.getContractFactory('ProjectJ');
-    const nftContract = await nftContractFactory.deploy(moderators,pausers,baseURI,tester1.address,degens);
-    await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
-
-    let mint;
-    mint = await nftContract.connect(tester2).mint({value: hre.ethers.utils.parseEther('0.1')});
-    console.log("Minted")
-
-    mint = await nftContract.mint({value: hre.ethers.utils.parseEther('0.1')});
-    console.log("Minted")
-
+    // Attach the proxy address to the ABI
+    const projectJ = await ProjectJ.attach('0xe3B72c01DC44d0C6925E776471bFdFd4A1CD7112')
+    
     let txn;
-    txn = await nftContract.tokenURI(1);
-    console.log("Token URI: ",txn);
-    txn = await nftContract.tokenURI(2);
-    console.log("Token URI: ",txn);
-    txn = await nftContract.degenimals(degen1.address);
-    console.log("Degen status of %s: %s",degen1.address,txn)
-    txn = await nftContract.degenimals(tester1.address);
-    console.log("Degen status of %s: %s",tester1.address,txn)
-
-    // let burn;
-    // burn = await nftContract.burn(0);
-    // console.log("Burned");
-    // balanceOf = await nftContract.balanceOf(tester1.address);
-    // console.log("Balance of %s address is: %d",tester1.address,balanceOf);
-
+    // Change this function to change the payload sent to contract
+    txn = await projectJ.mint({value: hre.ethers.utils.parseEther('0.01')});
+    console.log(txn)
 
 };
   
